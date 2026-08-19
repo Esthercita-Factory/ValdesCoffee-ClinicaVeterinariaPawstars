9f51234f739e16d4cacad5e72ee0a8e37bddbd35
ValdesCoffee <valdes.2022806@ensdbexcelencia.edu.co>
2026-08-19 14:07:20 -0500

    estos cambios de este proyecto nada que ver, probablemente dañaran un poco mas el proyecto de lo que deberia. Empezando porque me estoy pasando el POO por el arco del triunfo, tenganme fe porque yo la tengo xd

--- ClinicaVeterinariaPawstars/ ClinicaVeterinariaPawstars.API/Controllers/PetsController.cs
+++ ClinicaVeterinariaPawstars/ ClinicaVeterinariaPawstars.API/Controllers/PetsController.cs
@@ -1,48 +1,57 @@
-using Microsoft.AspNetCore.Mvc; 
-using ClinicaVeterinariaPawstars.Models;
-// Asumimos que tienes una carpeta Services
-using ClinicaVeterinariaPawstars.Services; 
-
-namespace ClinicaVeterinariaPawstars.Controllers;
-
-[ApiController]
-// TRUCO: Usa [controller] entre corchetes para que C# tome el nombre de la clase (Pets) automáticamente
-[Route("api/[controller]")] 
-public class PetsController : ControllerBase
+using Microsoft.AspNetCore.Mvc;
+using ClinicaVeterinariaPawStars.Domain.Entities;
+using ClinicaVeterinariaPawStars.Domain.Interfaces;
+public class PetsController : Controller
 {
-    // 1. Corrección: 'readonly' (con 'a') y debe ser del tipo de la Interfaz
     private readonly IPetService _petService;
-
-    // 2. Corrección: El constructor asigna la interfaz al campo privado
-    public PetsController(IPetService petService)
+}
+namespace ClinicaVeterinariaPawStars.Domain.Interfaces
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PetsController : ControllerBase
     {
-        _petService = petService;
-    }
-    
-    [HttpGet] 
-    // 3. Corrección: 'IActionResult' (todo pegado)
-    public IActionResult GetAll() => Ok(_petService.GetAll());
+        private readonly IPetService _petService;
 
-    // 4. Corrección: El nombre en la ruta {uuid} debe ser igual al del parámetro
-    [HttpGet("{uuid}")] 
-    public IActionResult GetByUid(string uuid) 
-    { 
-        var pet = _petService.GetByUuid(uuid);
-        return pet == null ? NotFound() : Ok(pet); 
-    }
+        public PetsController(IPetService petService)
+        {
+            _petService = petService;
+        }
 
-    [HttpPost] 
-    public IActionResult Create([FromBody] Pet pet) 
-    { 
-        _petService.Create(pet);
-        return CreatedAtAction(nameof(GetByUid), new { uuid = "generado-id" }, pet);
-    }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_petService.GetAll());
+        }
 
-    [HttpDelete("{uuid}")] 
-    // 5. Corrección: 'IActionResult' con 'A' mayúscula
-    public IActionResult Delete(string uuid) 
-    { 
-        _petService.Delete(uuid);
-        return NoContent(); 
+        [HttpGet("{uuid}")]
+        public IActionResult GetByUid(string uuid)
+        {
+            var pet = _petService.GetByUuid(uuid);
+
+            return pet == null
+                ? NotFound()
+                : Ok(pet);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Pet pet)
+        {
+            _petService.Create(pet);
+
+            return CreatedAtAction(
+                nameof(GetByUid),
+                new { uuid = pet.Uuid },
+                pet
+            );
+        }
+
+        [HttpDelete("{uuid}")]
+        public IActionResult Delete(string uuid)
+        {
+            _petService.Delete(uuid);
+
+            return NoContent();
+        }
     }
 }
---
+++ ClinicaVeterinariaPawstars/ClinicaVeterinariaPawStars.Domain/Interfaces/IPetServices.cs
@@ -0,0 +1,6 @@
+namespace ClinicaVeterinariaPawStars.Domain.Interfaces;
+
+public interface IPetService
+{
+    ClinicaVeterinariaPawStars.Domain.Entities.Pet GetPet();
+}
